989a7a097959b27ed369dbc2f83e9422012b6042
Oskar-hub <114953385+Oskar-hub@users.noreply.github.com>
2022-11-14 19:13:18 +0100

    Add files via upload

--- text.js
+++ text.js
@@ -10,9 +10,10 @@ function ChangeText1(){
     document.getElementById("text1").innerHTML = textElement[counter];
 
     document.getElementById("text").style.opacity = 0;
-
+ 
 
     document.getElementById("text1").style.opacity = 1;
+   
     
     setTimeout(ChangeText1, 2000);
 }
@@ -22,8 +23,10 @@ function ChangeText(){
     document.getElementById("text").innerHTML = textElement[counter];
 
     document.getElementById("text").style.opacity = 1;
+   
   
     document.getElementById("text1").style.opacity = 0;
+   
     
     setTimeout(ChangeText1, 2000);
 }
