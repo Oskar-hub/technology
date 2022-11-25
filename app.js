e11d853445045b374ea8acf6a26928a24abb5b9f
Oskar-hub <114953385+Oskar-hub@users.noreply.github.com>
2022-11-25 14:17:12 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-let person = document.querySelector('.ziutek');
+window.addEventListener('scroll', unmask);
+function unmask(){
+     let unmask = document.querySelectorAll('.unmask');
 
-person.addEventListener('mousedown', ()=>{
-     person.classList.toggle('statement');
-})
+     for(let i = 0; i < unmask.length; i++) { 
+          let height = window.innerHeight;
+          let top = unmask[i].getBoundingClientRect().top;
+          let point = 190;
+         
+          if(top < height - point){
+               unmask[i].classList.add('unmask-active');
+          }
+     }
+     }
+
+    
